3dfcac88df1747ed7968d18cb11286a21a7eaca1
aleksej24-ki <aleksejp@live.de>
2025-07-08 06:12:28 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,8 @@ def index():
     return "Бот работает!", 200
 
 # Устанавливаем webhook при запуске сервера
-@app.before_first_request
-def setup_webhook():
+
+if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url=f"{WEBHOOK_URL}/{BOT_TOKEN}")
+    bot.set_webhook(url=f"{WEBHOOK_URL}/{TOKEN}")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
